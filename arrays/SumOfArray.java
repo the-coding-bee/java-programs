918d76b785bb928ef921051041368d8c3839a7f7
the-coding-bee <studymania159@gmail.com>
2024-02-20 10:50:21 +0530

    sum of arrays

---
+++ arrays/SumOfArray.java
@@ -0,0 +1,14 @@
+
+public class SumOfArray {
+
+	public static void main(String[] args) {
+		int[] numbers = { 1, 2, 3, 4, 5 };
+		int sum = 0;
+
+		for (int number : numbers)
+			sum += number;
+
+		System.out.println("Sum = " + sum);
+	}
+
+}
