ba08a22598867d51c033e29f7cb062be916e29e7
the-coding-bee <studymania159@gmail.com>
2024-02-21 18:07:40 +0530

    lenght of array

---
+++ arrays/LengthOfArray.java
@@ -0,0 +1,8 @@
+public class LengthOfArray {
+
+	public static void main(String[] args) {
+		int[] numbers = { 1, 2, 3, 4, 5 };
+
+		System.out.println("Length Of Array = " + numbers.length);
+	}
+}
